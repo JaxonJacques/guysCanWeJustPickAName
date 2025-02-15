99266cdccec040bc7186d29d407164ab8c5e824e
VredstarX <130722153+VredstarX@users.noreply.github.com>
2025-02-15 16:18:06 -0700

    Everything about UI

---
+++ ReMind/ReMind/MainScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreen.swift
+//  ReMind
+//
+//  Created by Jessu Doroy on 2025-02-15.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainScreen()
+}
---
+++ ReMind/ReMind/Page2.swift
@@ -0,0 +1,18 @@
+//
+//  Page2.swift
+//  ReMind
+//
+//  Created by Jessu Doroy on 2025-02-15.
+//
+
+import SwiftUI
+
+struct Page2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Page2()
+}
